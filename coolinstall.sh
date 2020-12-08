48a1cb1d8167cbffb84415cda07cd4296fd794ed
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2020-12-08 16:37:14 +1100

    Update coolinstall.sh

--- coolinstall.sh
+++ coolinstall.sh
@@ -272,16 +272,18 @@ function check_os_comp {
 # This is the OpeningMessage, it is displayed after the FQDN has been received and shows the user all the information that will need throughout the install
 OpeningMessage() {
   clear
-  echo -e "\n${BLUE}Pterodactyl Installation Script"
-  echo -e "${DARK_GRAY}Created by MrFlacko - Inspired by Vilhelmprytz\n"
+  echo -e "\n${BLUE}Pterodactyl Panel Installation Script"
+  echo -e "${DARK_GRAY}Copyright (C) 2018 - 2020, Vilhelm Prytz, <vilhelm@prytznet.se>, et al."
+  echo -e "${DARK_GRAY}https://github.com/vilhelmprytz/pterodactyl-installer"
   echo -e "${LIGHT_GREEN}Hello,"
-  echo "This script was designed to quickly run through the Pterodactyl" 
+  echo "This script isn't associated with the Official Pterodactyl Project"
+  echo "though is designed to quickly run through the Pterodactyl" 
   echo "install with as much ease to the user as possible."
   echo "This script will set the panel with ssl on the machine."
   echo "This screen you see is the system's information, I would copy this down to use"
   echo "throughout the setup, though it is up to you. If you're happy with the information"
   echo "you see on the screen. You can press [Enter] to start the installation."
-  echo -e "Best of luck - Flacko \n"
+  echo -e "Best of luck\n"
   echo -e "${DARK_GRAY}Pterodactyl Version:${LIGHT_BLUE} $pterodactyl_version"
 #  echo -e "${DARK_GRAY}Script Version:${LIGHT_BLUE} $Script_Version"
   echo -e "${DARK_GRAY}Ubuntu Version:${LIGHT_BLUE} $os_version"
